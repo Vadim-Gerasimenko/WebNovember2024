c2a04bd2b3c481d1be5e325aa4f9ed3d8012cd34
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2025-01-12 14:45:44 +0700

    add a solution using classes ES6

--- Classes/script.js
+++ Classes/script.js
@@ -35,4 +35,35 @@
         new Cat("Барсик").say();
         new Animal("Дамбо").say();
     })();
+
+    console.log("");
+
+    (function () {
+        class Animal {
+            constructor(name) {
+                this.name = name;
+            };
+
+            say() {
+                console.log(`${this.name} издает звук.`);
+            };
+        }
+
+        class Dog extends Animal {
+            say() {
+                console.log(`${this.name} лает.`);
+            };
+        }
+
+        class Cat extends Animal {
+            say() {
+                console.log(`${this.name} мяукает.`);
+            };
+        }
+
+        console.log("Решение задачи с использованием классов ES6:");
+        new Dog("Шарик").say();
+        new Cat("Барсик").say();
+        new Animal("Дамбо").say();
+    })();
 })();
