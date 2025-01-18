cf72d3e4b1620c32bec662758e623d91567ae740
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2025-01-19 00:06:03 +0700

    make a single function for processing input fields with contact details

--- PhoneBookjQuery/script.js
+++ PhoneBookjQuery/script.js
@@ -1,13 +1,32 @@
 $(document).ready(function () {
-    const contactAddingForm = $("#contact-adding-form");
-    const contactAddingFormInputFields = contactAddingForm.find("input");
+    const addingForm = $("#contact-adding-form");
+    const addingFormInputFields = $(addingForm).find("input");
 
-    const lastNameInputField = $("#last-name-input-field");
-    const firstNameInputField = $("#first-name-input-field");
-    const phoneNumberInputField = $("#phone-number-input-field");
+    const table = $("#contacts-table");
+    const tableRows = [];
 
-    const contactsTable = $("#contacts-table");
-    const noContactsInfoMessage = $("#no-contacts-info-message");
+    const emptyTableMessage = $("#empty-contacts-table-message");
+
+    const deleteModalElement = $("#contact-delete-modal");
+    const deleteModalDialog = new bootstrap.Modal(deleteModalElement);
+
+    const deleteModalElementLastNameLabel = $(deleteModalElement).find(".last-name");
+    const deleteModalElementFirstNameLabel = $(deleteModalElement).find(".first-name");
+    const deleteModalElementPhoneNumberLabel = $(deleteModalElement).find(".phone-number");
+
+    const editModalElement = $("#contact-edit-modal");
+    const editModalDialog = new bootstrap.Modal(editModalElement);
+    const editModalElementInputFields = $(editModalElement).find("input");
+
+    const lastNameEditInputField = $(editModalElement).find("#last-name-edit-input-field");
+    const firstNameEditInputField = $(editModalElement).find("#first-name-edit-input-field");
+    const phoneNumberEditInputField = $(editModalElement).find("#phone-number-edit-input-field");
+
+    const addingCollapse = new bootstrap.Collapse(addingForm, {
+        toggle: false
+    });
+
+    const addingCollapseCloseButton = $("#contact-adding-collapse-close-button");
 
     class Contact {
         constructor(lastName, firstName, phoneNumber) {
@@ -17,57 +36,135 @@ $(document).ready(function () {
         }
     }
 
-    const contactsTableRows = [];
+    addingCollapseCloseButton.click(function () {
+        addingCollapse.hide();
+
+        setTimeout(function () {
+            addingFormInputFields.each((_, field) => $(field).removeClass("is-invalid").val(""));
+        }, 500);
+    });
+
+    function removeInvalidClasses(elements) {
+        elements.each((_, element) => $(element).removeClass("is-invalid"));
+    }
 
     function addContact(contact) {
-        const contactIndex = contactsTableRows.length;
+        const contactIndex = tableRows.length;
 
         if (contactIndex === 0) {
-            noContactsInfoMessage.hide();
+            emptyTableMessage.hide();
         }
 
         const tableRow = $("<tr>")
-            .append($("<td>").text(contactIndex + 1))
-            .append($("<td>").text(contact.lastName))
-            .append($("<td>").text(contact.firstName))
-            .append($("<td>").text(contact.phoneNumber))
-            .append($("<td>"));
+            .append($("<td>").addClass("contact-position").text(contactIndex + 1))
+            .append($("<td>").addClass("last-name").text(contact.lastName))
+            .append($("<td>").addClass("first-name").text(contact.firstName))
+            .append($("<td>").addClass("phone-number").text(contact.phoneNumber))
+            .append($("<td>")
+                .append("<button>")
+                .attr("type", "button")
+                .attr("data-bs-toggle", "modal")
+                .attr("data-bs-target", "#contact-edit-modal")
+                .text("Изменить")
+                .addClass("btn btn-primary btn-sm d-inline-block border-0 ms-2")
+                .click(function () {
+                    removeInvalidClasses(editModalElementInputFields);
 
-        contactsTableRows.push(tableRow);
-        $(contactsTable).find("tbody").append(tableRow);
+                    const detailWithLastName = $(tableRow).find(".last-name");
+                    const detailWithFirstName = $(tableRow).find(".first-name");
+                    const detailWithPhoneNumber = $(tableRow).find(".phone-number");
+
+                    $(lastNameEditInputField).val($(detailWithLastName).text());
+                    $(firstNameEditInputField).val($(detailWithFirstName).text());
+                    $(phoneNumberEditInputField).val($(detailWithPhoneNumber).text());
+
+                    $(editModalElement).find(".save-changes-button").off().click(function () {
+                        const updatedContact = addContactDetails(editModalElementInputFields);
+
+                        if (updatedContact === null) {
+                            return;
+                        }
+
+                        $(detailWithLastName).text(updatedContact.lastName);
+                        $(detailWithFirstName).text(updatedContact.firstName);
+                        $(detailWithPhoneNumber).text(updatedContact.phoneNumber);
+
+                        editModalDialog.hide();
+                    });
+                })
+            )
+            .append($("<td>")
+                .append("<button>")
+                .attr("type", "button")
+                .attr("data-bs-toggle", "modal")
+                .attr("data-bs-target", "#contact-delete-modal")
+                .text("Удалить")
+                .addClass("btn btn-danger btn-sm d-inline-block border-0 ms-2")
+                .click(function () {
+                    deleteModalElementLastNameLabel.text($(tableRow).find(".last-name").text());
+                    deleteModalElementFirstNameLabel.text($(tableRow).find(".first-name").text());
+                    deleteModalElementPhoneNumberLabel.text($(tableRow).find(".phone-number").text());
+
+                    $(deleteModalElement).find(".delete-button").off().click(function () {
+                        const rowIndex = tableRows.indexOf(tableRow);
+
+                        tableRows.splice(rowIndex, 1);
+                        tableRow.remove();
+
+                        for (let i = rowIndex; i < tableRows.length; ++i) {
+                            $(tableRows[i]).find(".contact-position").text(i + 1);
+                        }
+
+                        if (tableRows.length === 0) {
+                            emptyTableMessage.show();
+                        }
+
+                        deleteModalDialog.hide();
+                    });
+                })
+            );
+
+        tableRows.push(tableRow);
+        $(table).find("tbody").append(tableRow);
     }
 
-    contactAddingForm.submit(function (e) {
+    addingForm.submit(function (e) {
         e.preventDefault();
 
-        let isValidFieldsValues = true;
-        contactAddingFormInputFields.each((_, inputField) => $(inputField).removeClass("is-invalid"));
+        const contact = addContactDetails(addingFormInputFields);
 
-        const lastName = lastNameInputField.val().trim();
-
-        if (lastName.length === 0) {
-            $(lastNameInputField).addClass("is-invalid");
-            isValidFieldsValues = false;
-        }
-
-        const firstName = firstNameInputField.val().trim();
-
-        if (firstName.length === 0) {
-            $(firstNameInputField).addClass("is-invalid");
-            isValidFieldsValues = false;
-        }
-
-        const phoneNumber = phoneNumberInputField.val().trim();
-
-        if (phoneNumber.length === 0) {
-            $(phoneNumberInputField).addClass("is-invalid");
-            isValidFieldsValues = false;
-        }
-
-        if (!isValidFieldsValues) {
+        if (contact == null) {
             return;
         }
 
-        addContact(new Contact(lastName, firstName, phoneNumber));
+        addContact(contact);
+        addingCollapseCloseButton.click();
     });
+
+    function addContactDetails(inputFields) {
+        removeInvalidClasses(inputFields);
+
+        function getInputFieldValue(inputField) {
+            const value = $(inputField).val().trim();
+
+            if (value.length === 0) {
+                $(inputField).addClass("is-invalid");
+                isValidFieldsValues = false;
+            }
+
+            return value;
+        }
+
+        let isValidFieldsValues = true;
+
+        const lastName = getInputFieldValue(inputFields[0]);
+        const firstName = getInputFieldValue(inputFields[1]);
+        const phoneNumber = getInputFieldValue(inputFields[2]);
+
+        if (!isValidFieldsValues) {
+            return null;
+        }
+
+        return new Contact(lastName, firstName, phoneNumber);
+    }
 });
