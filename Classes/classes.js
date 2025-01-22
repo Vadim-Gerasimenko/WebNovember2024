98dc38737b07fbeeb7f0d6f01a8607433c8ecd84
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2025-01-22 15:15:14 +0700

    split the code into two files with subtasks

---
+++ Classes/classes.js
@@ -0,0 +1,29 @@
+(function () {
+    class Animal {
+        constructor(name) {
+            this.name = name;
+        }
+
+        say() {
+            console.log(`${this.name} издает звук.`);
+        }
+    }
+
+    class Dog extends Animal {
+        say() {
+            console.log(`${this.name} лает.`);
+        }
+    }
+
+    class Cat extends Animal {
+        say() {
+            console.log(`${this.name} мяукает.`);
+        }
+    }
+
+    console.log("Решение задачи с использованием классов ES6:");
+
+    new Dog("Шарик").say();
+    new Cat("Барсик").say();
+    new Animal("Дамбо").say();
+})();
