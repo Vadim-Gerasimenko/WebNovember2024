7b59c1494ef72f7a098fddbbb4a52d15f053ef66
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2025-01-22 15:14:33 +0700

    split the code into two files with subtasks

---
+++ Classes/constructorsAndPrototypes.js
@@ -0,0 +1,37 @@
+(function () {
+    "use strict";
+
+    function Animal(name) {
+        this.name = name;
+    }
+
+    Animal.prototype.say = function () {
+        console.log(`${this.name} издает звук.`);
+    };
+
+    function Dog(name) {
+        Animal.call(this, name);
+    }
+
+    Object.setPrototypeOf(Dog.prototype, Animal.prototype);
+
+    Dog.prototype.say = function () {
+        console.log(`${this.name} лает.`);
+    };
+
+    function Cat(name) {
+        Animal.call(this, name);
+    }
+
+    Object.setPrototypeOf(Cat.prototype, Animal.prototype);
+
+    Cat.prototype.say = function () {
+        console.log(`${this.name} мяукает.`);
+    };
+
+    console.log("Решение задачи с использованием конструкторов и прототипов:");
+
+    new Dog("Шарик").say();
+    new Cat("Барсик").say();
+    new Animal("Дамбо").say();
+})();
