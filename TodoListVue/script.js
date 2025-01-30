18a72687ed56ac7f5531e64fb5d36e7f3e7bd359
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2025-01-31 00:31:07 +0700

    add logic for modal dialog and list-item components

--- TodoListVue/script.js
+++ TodoListVue/script.js
@@ -1,10 +1,11 @@
-Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
             newTodoItemId: 1,
             newTodoItemText: "",
             isNewTodoItemTextInvalid: false,
-            items: []
+            items: [],
+            deleteItemIndex: 0
         };
     },
 
@@ -21,39 +22,157 @@ Vue.createApp({
 
             this.items.push({
                 id: this.newTodoItemId,
-                text: newTodoItemText,
-                isEditing: false,
-                editingText: newTodoItemText,
-                isEditingTextInvalid: false
+                text: newTodoItemText
             });
 
             this.newTodoItemId++;
             this.newTodoItemText = "";
         },
 
+        showDeleteTodoItemConfirm(itemIndex) {
+            this.deleteItemIndex = itemIndex;
+            this.$refs.confirmDeleteModal.show();
+        },
+
         deleteTodoItem(itemIndex) {
-            this.items.splice(itemIndex, 1);
+            this.items.splice(this.deleteItemIndex, 1);
+            this.$refs.confirmDeleteModal.hide();
         },
 
-        editTodoItem(itemIndex) {
-            const item = this.items[itemIndex];
-            item.editingText = item.text;
-            item.isEditing = true;
-        },
-
-        saveTodoItem(itemIndex) {
-            const item = this.items[itemIndex];
-
-            item.isEditingTextInvalid = false;
-
-            const editingText = item.editingText;
-
-            if (editingText.length === 0) {
-                item.isEditingTextInvalid = true;
-                return;
-            }
-            item.text = editingText;
-            item.isEditing = false;
+        saveTodoItem(itemIndex, newText) {
+            this.items[itemIndex].text = newText;
         }
     }
-}).mount("#app");
+});
+
+app.component("TodoItem", {
+    props: {
+        item: {
+            type: Object,
+            required: true
+        },
+
+        index: {
+            type: Number,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            isEditing: false,
+            isEditingTextInvalid: false,
+            editingText: this.item.text
+        };
+    },
+
+    methods: {
+        editTodoItem() {
+            this.editingText = this.item.text;
+            this.isEditing = true;
+        },
+
+        saveTodoItem() {
+            this.isEditingTextInvalid = false;
+
+            const editingText = this.editingText;
+
+            if (editingText.length === 0) {
+                this.isEditingTextInvalid = true;
+                return;
+            }
+
+            this.$emit("save", this.index, editingText);
+
+            this.isEditing = false;
+        }
+    },
+
+    template: `
+      <li class="mb-2">
+        <template v-if="isEditing">
+          <form @submit.prevent="saveTodoItem" class="row">
+            <div class="col-8 me-2">
+              <input v-model.trim="editingText"
+                     :class="{'is-invalid': isEditingTextInvalid}"
+                     class="form-control form-control-sm"
+                     type="text">
+              <div class="invalid-feedback">Необходимо задать значение</div>
+            </div>
+            <div class="col-auto ms-auto">
+              <button class="btn btn-outline-primary btn-sm border-0 me-1" type="submit">
+                Сохранить
+              </button>
+              <button @click="isEditing = false"
+                      class="btn btn-outline-danger btn-sm border-0"
+                      type="button">
+                Отмена
+              </button>
+            </div>
+          </form>
+        </template>
+        <template v-else>
+          <div class="row">
+            <div v-text="item.text" class="col-8 list-item-text"></div>
+            <div class="col-auto ms-auto">
+              <button @click="editTodoItem"
+                      class="btn btn-outline-primary btn-sm border-0 me-1"
+                      type="button">
+                Редактировать
+              </button>
+              <button @click="$emit('delete', index)"
+                      class="btn btn-outline-danger btn-sm border-0"
+                      type="button">
+                Удалить
+              </button>
+            </div>
+          </div>
+        </template>
+      </li>
+    `
+});
+
+app.component("Modal", {
+    data() {
+        return {
+            instance: null
+        };
+    },
+
+    mounted() {
+        this.instance = new bootstrap.Modal(this.$refs.modal);
+    },
+
+    methods: {
+        show() {
+            this.instance.show();
+        },
+
+        hide() {
+            this.instance.hide();
+        }
+    },
+
+    template: `
+      <div ref="modal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">
+                <slot name="header"></slot>
+              </h5>
+            </div>
+            <div class="modal-body">
+              <slot></slot>
+            </div>
+            <div class="modal-footer">
+              <slot name="action-button"></slot>
+              <button @click="hide" type="button" class="btn btn-sm btn-secondary">Отмена</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+});
+
+app.mount("#app");
