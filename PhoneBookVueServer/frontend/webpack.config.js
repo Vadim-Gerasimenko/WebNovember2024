6007a32b44368ed4a4893119f85e14098561944b
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2025-02-04 15:30:46 +0700

    configure wwbpack

---
+++ PhoneBookVueServer/frontend/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {VueLoaderPlugin} = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./js/script.js",
+    output: {
+        path: path.resolve(__dirname, "..", "public"),
+        filename: "script.js",
+        assetModuleFilename: "[path][name][ext]?[contenthash]"
+    },
+    devtool: "source-map",
+    target: ["web", "es5"],
+    devServer: {
+        hot: true,
+        open: true,
+        proxy: [
+            {
+                context: ["/api"],
+                target: "http://localhost:3000"
+            }
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2)$/,
+                type: "asset/resource"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.vue$/,
+                use: "vue-loader"
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        }),
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        }),
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: "true",
+            __VUE_PROD_DEVTOOLS__: "false",
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false"
+        })
+    ]
+};
