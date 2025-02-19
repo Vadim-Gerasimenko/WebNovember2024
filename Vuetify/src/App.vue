5e3978460cd65a2302b3882ae8e832b103617207
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2025-02-19 21:25:38 +0700

    create app

---
+++ Vuetify/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <default></default>
+  </v-app>
+</template>
+
+<script setup>
+  //
+import Default from "@/layouts/default.vue";
+</script>
---
+++ Vuetify/src/main.js
@@ -0,0 +1,20 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import {registerPlugins} from "@/plugins";
+
+// Components
+import App from "./App.vue";
+
+// Composables
+import {createApp} from "vue";
+import {useAppStore} from "@/stores/app.js";
+
+const app = createApp(App);
+registerPlugins(app);
+
+app.mount("#app");
